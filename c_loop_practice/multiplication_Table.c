182874c1579726c1a0f7dcf791b1da887b4132f3
tauhidul252 <tauhidulislam252@gmail.com>
2024-10-12 23:45:33 +0600

    Multiplication Table Nested Loop use user input system

--- c_loop_practice/multiplication_Table.c
+++ c_loop_practice/multiplication_Table.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
  int main(){
 
+/*
     for (int i = 1; i < 6; i++)
     {
 
@@ -12,6 +13,22 @@
         printf("\n");
         
     }
+    */
+
+
+    int A;
+    scanf("%d",&A);
+
+
+  for (int i = 1; i <=A; i++)
+  {
+    for (int j = 1; j <=5; j++)
+    {
+        printf("%d * %d = %d \n", i, j, i*j);
+    }
+    printf("\n\n");
+
+  }
     
 
 
