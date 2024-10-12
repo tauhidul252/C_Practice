8f3cbe71cf828b77ccb1d17bb3e9a87038e10629
tauhidul252 <tauhidulislam252@gmail.com>
2024-10-12 22:01:36 +0600

    Initial commit

---
+++ c_loop_practice/loop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    for (int i = 0; i <=5; i++)
+    {
+
+        for (int j = 0; j <=4; j++)
+        {
+          printf("%d ", j); 
+        }// test comment
+        
+        printf("\n");
+        
+    }
+    
+
+
+
+    return 0;
+}
