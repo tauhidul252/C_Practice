830684a1a7698fe47ce8bfd674adb0dd9dd04279
tauhidul252 <tauhidulislam252@gmail.com>
2024-10-14 11:13:27 +0600

    Assignment 2.4 Complete

---
+++ Assignment/Assignment 02/Pattern_again.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+
+    int N;
+    scanf("%d",&N);
+
+    for (int i = 1; i <=N; i++)
+    {
+        for (int j = 1; j <=i; j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+         
+    }
+
+    for (int i = 1; i <=N; i++)
+    {
+        for (int j=1; j<=i; j++)
+               printf(" ");
+    {
+        for (int j = 1; j <=N-i; j++)
+        {
+           printf("%d", j); 
+
+
+        }
+        
+        printf("\n");
+         
+    }
+    }
+    
+    
+
+    return 0;
+}
