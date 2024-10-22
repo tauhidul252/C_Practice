4da664a03488d421d5e95f4d17f3b429b43eb56f
tauhidul252 <tauhidulislam252@gmail.com>
2024-10-22 12:35:56 +0600

    Array Input

--- testAllCode.c
+++ testAllCode.c
@@ -1,8 +1,17 @@
-#include <stdio.h>
+// #include <stdio.h>
+// int main()
+// {
+//  int a =(int) 50.45, b = 60, sum;
+//  sum = a + b;
+//  printf("%d + %d = %d", a, b, sum);
+//  return 0;
+// } 
+
+#include<stdio.h>
 int main()
 {
- int a =(int) 50.45, b = 60, sum;
- sum = a + b;
- printf("%d + %d = %d", a, b, sum);
- return 0;
-} 
+    char str[20];
+    scanf("%s",&str);
+    printf("%s",str);
+    return 0;
+}
