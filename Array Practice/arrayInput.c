1508a51855ce516a1895c0cfdcfea2dbe47a236e
tauhidul252 <tauhidulislam252@gmail.com>
2024-10-23 00:05:11 +0600

    array char input

--- Array Practice/arrayInput.c
+++ Array Practice/arrayInput.c
@@ -2,21 +2,34 @@
 
 int main(){
 
-    int arr[5];
-     
+    /* int arr[5];
 
     for (int i = 0; i <5; i++)
     {
        scanf("%d",&arr[i]); 
+
     }
-    
 
     for (int i = 0; i <5; i++)
     {
-       printf("%d ",arr[i]);
+       printf("%d ",&arr[i]);
  
     }
-    
+    */
+
+   char arr[5];
+
+    for (int i = 0; i <5; i++)
+    {
+       scanf("%c",&arr[i]); 
+
+    }
+
+    for (int i = 0; i <5; i++)
+    {
+       printf("%c ",arr[i]);
+ 
+    }
     
 
     return 0;
