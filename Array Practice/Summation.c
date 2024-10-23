1dd22a8c00674ebadffb42f7d8f1b417aa964cdd
tauhidul252 <tauhidulislam252@gmail.com>
2024-10-23 11:53:27 +0600

    Summation And Avg using Type Casting

--- Array Practice/Summation.c
+++ Array Practice/Summation.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
-int main(){
+int main()
+{
 
     int N;
-    scanf("%d",&N);
+    scanf("%d", &N);
 
     int a[N];
 
-    for (int i = 0; i <N; i++)
+    for (int i = 0; i < N; i++)
     {
-       scanf("%d",&a[i]);
+        scanf("%d", &a[i]);
     }
 
     int sum = 0;
-    
+
     for (int i = 0; i < N; i++)
     {
 
-       sum += a[i];
+        sum += a[i];
     }
-    
 
- printf("%d \n",sum);
+    printf("%d \n", sum);
+
+    double vag = (double)sum / N; // type casting
+
+    printf("%lf", vag);
+
     return 0;
-
 }
