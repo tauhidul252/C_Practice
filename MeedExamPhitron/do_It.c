78bc29497ffcfe0d1050f618d515be9cf4901031
tauhidul252 <tauhidulislam252@gmail.com>
2024-10-24 21:21:59 +0600

    Do It problem Sloved add Test case condition

--- MeedExamPhitron/do_It.c
+++ MeedExamPhitron/do_It.c
@@ -5,15 +5,26 @@ int main(){
     int N,K;
     scanf("%d %d",&N,&K);
 
-    for (int i = 1; i <=N; i++)
+
+    if (N<=100 && K<=100)
+    
     {
+         for (int i = 1; i <=N; i++)
+     {
         for (int j = 1; j <=K; j++)
         {
             printf("%d ",j);
         }
         printf("\n");
         
+     }
+
     }
+    
+
+
+
+   
 
 
     return 0;
