54179edf3c32ac891a62fdc3dd23aced19e8ae3b
tauhidul252 <tauhidulislam252@gmail.com>
2024-10-28 18:48:27 +0600

    with array valu

--- 2D Array/array_2D.c
+++ 2D Array/array_2D.c
@@ -2,12 +2,12 @@
 
 int main(){
 
-    int arr[3][3];
+    int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            printf("i = %d j = %d addres =  %d ||",i,j,arr);
+            printf("i = %d j = %d addres =  %d ||",i,j,arr[i][j]);
         }
         printf("\n");
     }
