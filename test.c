15ad407151b4241287e32eef52476e035d181eb4
tauhidul252 <tauhidulislam252@gmail.com>
2024-11-15 23:14:03 +0600

    contest work

--- test.c
+++ test.c
@@ -1,43 +1,18 @@
 #include <stdio.h>
-#include <string.h>
+
 
 int main()
 {
+    int N,count=0;
+    scanf("%d", &N);
 
-    char S1[101], S2[101], S3[101];
-
-    scanf("%s %s %s", S1, S2, S3);
-
-    int len = strlen(S1);
-    
-    int count = 0;
-
-    for (int i = 0; i < len; i++)
+    for (int i = 2; i <=N; i+=2)
     {
-        char C1 = S1[i];
-        char C2 = S2[i];
-        char C3 = S3[i];
-
-        if (C1 == C2 && C2 == C3)
-        {
-            continue;
-        }
-
-
-        else if (C1 == C2 || C1 == C3 || C2 == C3)
-        {
-            count += 1;
-        }
-
-
-        else
-        {
-            count += 2;
-        }
-
-
+        count+=i;
     }
+    
 
+    
 
     printf("%d", count);
 
